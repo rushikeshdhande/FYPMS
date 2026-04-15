1bbff863c186494ce0590587824a473759a5bea4
Rushikesh Dhande <rushikeshdhande59@gmail.com>
2026-04-16 03:08:10 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -100,7 +100,7 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
 
         {/* Student Routes */}
         <Route
